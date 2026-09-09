e871d18e9af906364f7351c76e41cd209748c32a
Rupanjan Mukherjee <rupanjanmukherjee686@gmail.com>
2026-09-09 19:13:27 +0530

    Use same-origin production API

--- src/api.js
+++ src/api.js
@@ -4,4 +4,4 @@ const isLocalhost =
 
 export const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  (isLocalhost ? "http://localhost:5000" : "https://dhara-main.onrender.com")
+  (isLocalhost ? "http://localhost:5000" : window.location.origin)
